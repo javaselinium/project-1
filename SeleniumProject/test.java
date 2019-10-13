825af66b9c1751dff513064ce0e625b787ebf616
user <user@user-PC>
2019-10-13 15:57:12 +0530

    added method m1

--- SeleniumProject/test.java
+++ SeleniumProject/test.java
@@ -1,7 +1,12 @@
 
 public class test {
 
-	public static void main(String[] args) {
+	public void m1()
+	{
+		
+	}
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 
 	}
